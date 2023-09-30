ffed00da2c789762546b632d0874fc80cb0291c4
Marco Fratini <122683263+marzboi@users.noreply.github.com>
2023-09-30 10:17:54 +0200

    Saving

--- src/app/gifs/gifs.module.ts
+++ src/app/gifs/gifs.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
+
 import { CommonModule } from '@angular/common';
+import { SharedModule } from '../shared/shared.module';
+
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { SearchBoxComponent } from './components/search-box/search-box.component';
 import { CardListComponent } from './components/card-list/card-list.component';
@@ -12,7 +15,7 @@ import { CardComponent } from './components/card/card.component';
     CardListComponent,
     CardComponent,
   ],
-  imports: [CommonModule],
+  imports: [CommonModule, SharedModule],
   exports: [HomePageComponent],
 })
 export class GifsModule {}
